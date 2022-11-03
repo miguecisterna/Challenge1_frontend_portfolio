97cb6a622fd6480424fa7a70bcba0ef75d7e8acc
Migue Cisterna <miguecisterna@gmail.com>
2022-11-02 21:16:24 -0300

    Agregado archivo php para contact-form

---
+++ contactFormHandler.php
@@ -0,0 +1,25 @@
+<?php
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
+
+
+    $email_from = 'miguecisterna@gmail.com';
+
+    $email_subject = "New Form Submission";
+
+    $email_body = "User Name: $name.\n".
+                    "User Email: $visitor_email.\n".
+                      "User Message: $message.\n";
+    
+    $to = "miguecisterna@gmail.com";
+
+    $headers = "From: $email_from \r\n";
+
+    $headers .= "Reply-To: $visitor_email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("Location: index.html");
+    
+?>
